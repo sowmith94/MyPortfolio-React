584dc3e218ea610045d7973383eabc8b05f857b1
mariuma19 <marium.a@globalmonitor.us>
2021-01-15 15:59:11 +0530

    Projects added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Nav from './components/Nav'
 import About from './components/About'
 import Projects from './components/Projects';
 import Banner from './components/Banner';
+import Skills from './components/Skills';
 
 function App() {
   const personalInfo={
@@ -16,12 +17,37 @@ function App() {
     email: 'someone@something.com'
   }
 
+  const projectList=[
+    {
+      name: 'Project 1',
+      company: "The International Org",
+      descp: 'did something and created a website for the addition of something. Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora incidunt expedita vero debitis minima reiciendis nobis officiis harum a amet dolorum hic aspernatur esse ea ut modi, totam molestiae quo?'
+    },
+    {
+      name: 'Project 2',
+      company: "The very big company",
+      descp: 'did something and created a website for the addition of something Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora incidunt expedita vero debitis minima reiciendis nobis officiis harum a amet dolorum hic aspernatur esse ea ut modi, totam molestiae quo?'
+    },
+    {
+      name: 'Project 3',
+      company: "The very big company",
+      descp: 'did something and created a website for the addition of something Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora incidunt expedita vero debitis minima reiciendis nobis officiis harum a amet dolorum hic aspernatur esse ea ut modi, totam molestiae quo?'
+    },
+    {
+      name: 'Project 3',
+      company: "The very big company",
+      descp: 'did something and created a website for the addition of something Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora incidunt expedita vero debitis minima reiciendis nobis officiis harum a amet dolorum hic aspernatur esse ea ut modi, totam molestiae quo?'
+    },
+  ]
+
   return (
     <div className="App">
       <Nav />
       <Banner personalInfo={personalInfo} />
       <About personalInfo={personalInfo}/>
-      <Projects />
+      <Projects projectList={projectList}/>
+      <Skills />
+      <p></p>
     </div>
   );
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,9 +1,15 @@
 import React from 'react'
 
-const Projects = () => {
+const Projects = ({projectList}) => {
     return (
         <div className="projects-section" id="Projects">
-            <h2>Projects</h2>
+            {projectList.map(project => (
+                <div className="project-box">
+                    <h1>{project.name}</h1>
+                    <span>{project.company}</span>
+                    <p>{project.descp}</p>
+                </div>
+            ))}
         </div>
     )
 }
