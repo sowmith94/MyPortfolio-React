0b7fd3440d9e50984338cfcee6111f554f16101d
mariuma19 <marium.a@globalmonitor.us>
2021-01-20 13:22:20 +0530

    final changes

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -8,7 +8,7 @@ const Banner = ({personalInfo}) => {
         id="banner"
         >
             <div className="banner-info">
-                <h3>Welcome</h3>
+                <h3>Welcome!</h3>
                 <h1>I'm {fullname}</h1>
                 <p className="banner-descp">I am a student at Lorem ipsum dolor sit amet consectetur, adipisicing elit. c eveniet. Repudiandae et error officia nostrum possimus!</p>
                 <a href={facebook}><i className="fab fa-facebook-f"></i> </a>
